d0ee20717499a6076b9ef37c880ffacf5e201e39
Your Name <splinter928@gmail.com>
2022-06-20 22:00:52 +0500

    commit 20_06_2022_2

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ToDo
 
-# Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    """Add readonly field with creation date"""
+    readonly_fields = ('created',)
+
+
+admin.site.register(ToDo, TodoAdmin)
